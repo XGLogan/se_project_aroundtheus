3b0b43d21fb6df1f5aff910b51650f0de6b84112
XGLogano <ocplogan@gmail.com>
2025-01-15 15:20:58 -0500

    update for my projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Section from '../components/Section.js';
+import Section from '../components/Section.js'; 
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
 import PopupWithForm from '../components/PopupWithForm.js';
@@ -7,66 +7,120 @@ import UserInfo from '../components/UserInfo.js';
 import { initialCards, validationSettings } from '../components/utils.js';
 import '../pages/index.css';
 
-/** DOM Elements */
 
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileName = document.querySelector("#profile-name");
+const profileDescription = document.querySelector("#profile-description");
+const profileNameInput = document.querySelector("#profile-name-input");
+const profileDescriptionInput = document.querySelector("#profile-description-input");
+
+// Card & Card Form Elements
 const addNewCardButton = document.querySelector("#profile-add-button");
+const addCardModal = document.querySelector("#profile-add-card");
+const addCardFormElement = addCardModal.querySelector("#add-card-form");
 
-/** Instances */
-const userInfo = new UserInfo({
-  nameSelector: "#profile-name",
-  descriptionSelector: "#profile-description",
-});
+const profileEditForm = profileEditModal.querySelector("#profile-edit-form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTitleInput = addCardFormElement.querySelector("#add-card-name-input");
+const cardImageInput = addCardFormElement.querySelector("#add-card-image-input");
 
-const cardSection = new Section(
-  {
-    items: initialCards,
-    renderer: (data) => {
-      const card = new Card(data, "#card-template", (name, link) => {
-        imagePopup.open({ name, link });
-      });
-      const cardElement = card.getView();
-      cardSection.addItem(cardElement);
-    },
-  },
-  ".cards__list"
-);
 
-const profilePopup = new PopupWithForm("#profile-edit-modal", (data) => {
-  userInfo.setUserInfo(data);
-  profilePopup.close();
-});
+/* Functions */
+function closeModal(modal) {
+  modal.classList.remove('modal_opened');
+  document.removeEventListener("keydown", handleModalKeyDown);
+}
 
-const addCardPopup = new PopupWithForm("#profile-add-card", (data) => {
-  const card = new Card(data, "#card-template", (name, link) => {
-    imagePopup.open({ name, link });
-  });
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+  document.addEventListener("keydown", handleModalKeyDown);
+}
+
+function handleModalKeyDown(event) {
+  if (event.key === "Escape") {
+    const openedModal = document.querySelector(".modal_opened");
+    if (openedModal) {
+      closeModal(openedModal);
+    }
+  }
+}
+
+// Handle image click event
+function handleImageClick(name, link) {
+  const fullImage = previewImageModal.querySelector("#full-image");
+  const fullImageTitle = previewImageModal.querySelector("#full-image-title");
+  fullImage.src = link;
+  fullImage.alt = name;
+  fullImageTitle.textContent = name;
+  openModal(previewImageModal);
+}
+
+// Render a card
+function renderCard(cardData) {
+  const card = new Card(cardData, "#card-template", handleImageClick);
   const cardElement = card.getView();
-  cardSection.addItem(cardElement);
-  addCardPopup.close();
-});
+  cardListEl.prepend(cardElement);
+}
 
-const imagePopup = new PopupWithImages("#full-image-modal");
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closeModal(profileEditModal);
+}
+
+function handleAddCardSubmit(e) {
+  e.preventDefault();
+  const name = cardTitleInput.value;
+  const link = cardImageInput.value;
+  renderCard({ name, link });
+  closeModal(addCardModal);
+  addCardFormElement.reset();
+  addCardFormValidator.disableButton();
+}
 
 /** Event Listeners */
 profileEditButton.addEventListener("click", () => {
-  const userData = userInfo.getUserInfo();
-  document.querySelector("#profile-name-input").value = userData.name;
-  document.querySelector("#profile-description-input").value = userData.description;
-  profilePopup.open();
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent.trim();
+  openModal(profileEditModal);
 });
 
-addNewCardButton.addEventListener("click", () => addCardPopup.open());
+const modalCloseButtons = document.querySelectorAll('.modal__close-button');
+modalCloseButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
+function closeModalOnOverlayClick(event) {
+  if (event.target.classList.contains('modal')) {
+    closeModal(event.target); 
+  }
+}
+const modals = document.querySelectorAll('.modal');
+modals.forEach((modal) => {
+  modal.addEventListener('click', closeModalOnOverlayClick);  
+});
 
-profilePopup.setEventListeners();
-addCardPopup.setEventListeners();
-imagePopup.setEventListeners();
+// Close preview image modal
+const previewImageModal = document.querySelector("#full-image-modal");
+const closePreviewModalButton = document.querySelector("#close-modal-button");
+closePreviewModalButton.addEventListener("click", () => {
+  closeModal(previewImageModal);
+});
 
-/** Validation */
-const profileFormValidator = new FormValidator(validationSettings, document.querySelector("#profile-edit-form"));
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+
+addNewCardButton.addEventListener("click", () => openModal(addCardModal));
+
+initialCards.forEach((cardData) => renderCard(cardData));
+
+const profileFormValidator = new FormValidator(validationSettings, profileEditForm);
 profileFormValidator.enableValidation();
 
-const addCardFormValidator = new FormValidator(validationSettings, document.querySelector("#add-card-form"));
+const addCardFormValidator = new FormValidator(validationSettings, addCardFormElement);
 addCardFormValidator.enableValidation();
-
-/** Render Initial Cards */
-cardSection.renderItems();
