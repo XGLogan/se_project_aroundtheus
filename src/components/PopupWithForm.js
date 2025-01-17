6dbde5f26bda3949be326dd82e856844fef05d88
XGLogano <ocplogan@gmail.com>
2025-01-17 00:42:59 -0500

    Update For My Project

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,14 +4,12 @@ export default class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super(popupSelector);
     this._handleFormSubmit = handleFormSubmit;
-    // Grab the form inside the modal
     this._form = this._popup.querySelector('.modal__form');
-    // Grab all inputs inside the form
     this._inputList = this._form.querySelectorAll('.modal__input');
   }
 
   _getInputValues() {
-    // Build an object of { [input.name]: input.value }
+
     const inputValues = {};
     this._inputList.forEach((input) => {
       inputValues[input.name] = input.value;
